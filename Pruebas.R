9c1e5fe14d6533ec0dafd3e3c25d749523d4840e
IsmaelLuAl <56585358+IsmaelLuAl@users.noreply.github.com>
2020-02-21 14:50:50 +0100

    Delete Pruebas.R

--- Pruebas.R
+++
@@ -1,6 +0,0 @@
-activity$datetype <- sapply(activity$date, function(x) {
-    if (weekdays(x) == "Sábado" | weekdays(x) =="Domingo") 
-        {y <- "Weekend"} else 
-        {y <- "Weekday"}
-    y
-})
